5d79a9df4a84a8acd5ad7e992c537f5acebb9780
Marcos Volpi <marcos.volpi@BP4.com>
2020-11-20 22:13:08 -0300

    Add autoincrement

--- src/models/server.js
+++ src/models/server.js
@@ -2,7 +2,7 @@ const sequelize = require('../mysql');
 const message = require('./message');
 
 const Server = sequelize.define('servers', {
-  id: {type: Sequelize.SMALLINT, primaryKey: true},
+  id: {type: Sequelize.SMALLINT, primaryKey: true, autoIncrement: true},
   name: Sequelize.STRING,
 },
 {
