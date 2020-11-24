1a3e61ed589ca6c1a10ecbac318f25264222c697
Marcos Volpi <marcos.volpi@BP4.com>
2020-11-24 18:24:32 -0300

    Fix migration

--- src/migrations/20201122203141-create-server.js
+++ src/migrations/20201122203141-create-server.js
@@ -10,7 +10,8 @@ module.exports = {
       },
       server: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       description: {
         allowNull: false,
