a6df082b42a2dd8d7717e6848b6776e6540b4a71
Marcos Volpi <marcos.volpi@BP4.com>
2021-04-24 23:12:48 -0300

    Add price

--- src/seeders/20210406232000-demo-product.js
+++ src/seeders/20210406232000-demo-product.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Products', [
-        {name: 'jean', description: 'es un lindo jean', id_shop: 1, created_at: new Date(), updated_at: new Date()},
-        {name: 'micro short', description: 'es un lindo short', id_shop: 2, created_at: new Date(), updated_at: new Date()},
-        {name: 'remera', description: 'escote en V', id_shop: 3, created_at: new Date(), updated_at: new Date()},
+        {name: 'jean', description: 'es un lindo jean', price: 700, id_shop: 1, created_at: new Date(), updated_at: new Date()},
+        {name: 'micro short', description: 'es un lindo short', price: 250, id_shop: 2, created_at: new Date(), updated_at: new Date()},
+        {name: 'remera', description: 'escote en V', price: 320, id_shop: 3, created_at: new Date(), updated_at: new Date()},
     ]);
   },
 
