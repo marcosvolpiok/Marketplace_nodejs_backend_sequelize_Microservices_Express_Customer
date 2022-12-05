fa853379c1038d88e99e65f96d7b90b53f1aebe5
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-05 09:33:55 -0300

    Remove comment

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -5,8 +5,6 @@ const {
 const resolvers = {
   Query: {
       async getShop(root, args, context) {
-          //console.log('reqqq', context.req.url)
-          console.log('shopRepository ' + shopRepositoryOb)
           return await shopRepositoryOb.list(context.req);
       },
   },
