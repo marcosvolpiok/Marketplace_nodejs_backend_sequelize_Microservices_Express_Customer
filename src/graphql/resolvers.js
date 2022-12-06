cbdb9c4732959c1b55f5a3109ace568eef276505
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-06 08:24:35 -0300

    Refactor Get Shop

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,12 +1,12 @@
 const {
-  shopRepositoryOb, productRepositoryOb, customerServiceOb
+  shopServiceOb, productRepositoryOb, customerServiceOb
 } = require('../dependencies/');
 
 const resolvers = {
   Query: {
     async getShop(root, args, context) {
       context.req.url = 'getShop';
-      return await shopRepositoryOb.list(context.req);
+      return await shopServiceOb.list(context.req);
     },
 
     async getProduct(root, args, context) {
